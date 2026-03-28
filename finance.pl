29b844eee99d0e557212a50c7ed2331f543504f6
shrashti25bce10154-prog <shrashti.25bce10154@vitbhopal.ac.in>
2026-03-28 15:21:52 +0530

    Update finance.pl Added logic rules for filtering and budget alerts.

--- finance.pl
+++ finance.pl
@@ -3,3 +3,17 @@ expense(bus, 20, travel).
 expense(netflix, 499, subscription).
 expense(stationary, 100, study).
 expense(pizza, 300, food).
+list_category(Cat) :-
+    expense(Item, Cost, Cat),
+    format('~w: ~w~n', [Item, Cost]).
+
+is_expensive(Item) :-
+    expense(Item, Cost, _),
+    Cost > 200.
+
+budget_check(Item, 'Alert: High Spend') :-
+    is_expensive(Item).
+
+budget_check(Item, 'OK') :-
+    expense(Item, Cost, _),
+    Cost =< 200.
